f3a156a4ee0a11cced5f82a5d7f2b4764fc575a4
praydv <praveenyadavjpr@gmail.com>
2018-02-04 09:18:20 +0530

    Added echo

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #################################
-#				#
-#				#
+# Welcome to first script       #
+# Date: 04-02-2018              #
 #################################
+
+echo "This is a test script"
+echo "Thanks"
 
